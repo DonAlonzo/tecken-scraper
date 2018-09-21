42173c6d8692aff1709f32cbf9f107c3f6fe005d
Emil Boman <emil.boman@gmail.com>
2018-09-21 13:35:40 +0200

    automatic fps adjustment

--- scrape.py
+++ scrape.py
@@ -2,11 +2,14 @@ from bs4 import BeautifulSoup
 from requests import session
 import sys
 import re
+import os
 import moviepy.editor as mp
 from moviepy.video.fx.all import crop
 import pyvips
 
-if len(sys.argv) < 2: raise Exception('Missing arguments.')
+if len(sys.argv) < 2:
+  print('Missing arguments.')
+  exit(1)
 
 with session() as c:
   for word_id in sys.argv[1:]:
@@ -21,11 +24,21 @@ with session() as c:
     video_src = matches.group(1)
     video_url = "{}{}".format(url, video_src)
     clip = mp.VideoFileClip(video_url)
+    
     clip = clip.resize(height=400)
     (width, height) = clip.size
     if width > 540:
       clip = crop(clip, width=540, x_center=width/2)
-    clip.write_gif(animation_file, program='ffmpeg', fps=25)
+
+    fps = 25
+    done = False
+    while not done:
+      clip.write_gif(animation_file, program='ffmpeg', fps=fps)
+      if os.path.getsize(animation_file) > 7 * 1024 * 1024:
+        fps = fps - 1
+      else:
+        done = True 
+
 
     page = BeautifulSoup(result.text, "lxml")
     transcription = ''.join(page.find("div", {"class": "transfont"}).contents)
